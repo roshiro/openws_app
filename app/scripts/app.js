c2bd7f0ccc8c58c5c06a077cd07137e4d0573c54
Rafael Oshiro <rafael.oshiro@gmail.com>
2014-12-05 00:40:39 +0000

    Styling logos and images

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -37,7 +37,7 @@ angular
       .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl',
-        login: true
+        public: true
       })
       .otherwise({
         redirectTo: '/home'
@@ -46,30 +46,25 @@ angular
   .run(function($http, $log, $location, $rootScope, user) {
     user.init({ appId: '54707a9c6d212' });
     var redirectUrls = ["/signup", "/login", "/", ""];
-    if(redirectUrls.indexOf($location.$$path) > 0 && user.status().authenticated) {
+
+    if(($location.$$path == "" || $location.$$path == "#/") && !user.status().authenticated) {
+      $location.path('#/');
+    }
+
+    if(redirectUrls.indexOf($location.$$path) >= 0 && user.status().authenticated) {
+      $log.debug("redirecting");
       $location.path('/home');
     }
 
     $rootScope.$on('user.login', function() {
-      var userUrl = "https://openws.herokuapp.com/users?apiKey=2as343a80432",
+      var userUrl = "https://openws.herokuapp.com/api/users/new?apiKey=b9276e788652f8287e80242dab6e5a2a",
           userJson = {
             email: user.current.email,
-            user_id: user.current.user_id,
-            api_key: "ajs68723jl2l32"
-          },
-          query = JSON.stringify({ user_id : user.current.user_id });
+            user_id: user.current.user_id
+          };
 
-      $http.get(userUrl + "&q="+query).success(function(data) {
-        if(data.length == 0) {
-          $log.debug("user does not exist in DB");
-          $http.post(userUrl, userJson).success(function(data) {
-            $log.debug("saved");
-            $rootScope.apiKey = "ajs68723jl2l32";
-          });
-        } else {
-          $log.debug("user does exist in DB");
-          $rootScope.apiKey = data[0].api_key;
-        }
+      $http.post(userUrl, userJson).success(function(data) {
+        $rootScope.apiKey = data.api_key;
       });
     });
   });
